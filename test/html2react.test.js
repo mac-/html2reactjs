f36962a8b12e742e6f6b397572046ee5953c54ed
Mac Angell <mac.angell@pearson.com>
2014-09-14 23:52:58 -0600

    initial commit

---
+++ test/html2react.test.js
@@ -0,0 +1,11 @@
+var assert = require('assert'),
+	HTML2React = require('../lib/html2react.js');
+
+describe('HTML2React Unit Tests', function() {
+
+	it('should throw an error when 2 or more options use the same character', function(done) {
+
+		done();
+	});
+
+});
